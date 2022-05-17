380e97d4668bea0093eb22bb3710faa47998784d
deepakoraon19 <deepak.19ranchi@gmail.com>
2022-05-17 18:46:41 +0530

    slideShow problem

---
+++ index.js
@@ -0,0 +1,23 @@
+let slideShowImg = document.getElementsByClassName("ss-img");
+slideShowImg[0].classList.add("display");
+
+let slideShow = () => {
+  let i = 0;
+  let j = 0;
+  setInterval(() => {
+    if (i > slideShowImg.length - 1) {
+      console.log("UP " + i);
+      slideShowImg[i - 1].classList.remove("display");
+      i = 0;
+      slideShowImg[i].classList.add("display");
+    } else {
+      console.log(i);
+      slideShowImg[i].classList.remove("display");
+      if (i == slideShowImg.length - 1) {
+        slideShowImg[0].classList.add("display");
+      }
+      slideShowImg[++i].classList.add("display");
+    }
+  }, 1000);
+};
+slideShow();
