c3a91d811c4fd9640168375ef9fdb86ca2ba7db9
deepakoraon19 <deepak.19ranchi@gmail.com>
2022-06-29 17:47:45 +0530

    button on slide show

--- index.js
+++ index.js
@@ -2,39 +2,38 @@ let slideShowImg = document.getElementsByClassName("ss-img");
 let btn = document.getElementsByClassName("round-btn");
 slideShowImg[0].classList.add("display");
 btn[0].classList.add("grey");
-for(let z=0;z<btn.length;z++)
-{
-  btn[z].addEventListener('click',()=>{
-    i=z;
-  })
+let i = 0;
+let j = 0;
+for (let z = 0; z < btn.length; z++) {
+  j = j % slideShowImg.length;
+  btn[z].addEventListener("click", () => {
+    slideShowImg[j].classList.remove("display");
+    btn[j].classList.remove("grey");
+    i = z;
+    slideShowImg[i].classList.add("display");
+    btn[i].classList.add("grey");
+  });
 }
 
 let slideShow = () => {
-  let i = 0;
-  let j = 0;
   setInterval(() => {
-    j=i%slideShowImg.length;
+    j = i % slideShowImg.length;
     // console.log("Before  "+i,j);
-    
+
     slideShowImg[j++].classList.remove("display");
-    btn[j-1].classList.remove("grey");
-    
-    
-    if(j==slideShowImg.length-1)
-    {
+    btn[j - 1].classList.remove("grey");
+
+    if (j == slideShowImg.length) {
       slideShowImg[0].classList.add("display");
       btn[0].classList.add("grey");
-      i=0;   
-    }
-    else{
+      i = 0;
+    } else {
       slideShowImg[j].classList.add("display");
       btn[j].classList.add("grey");
       i++;
     }
-    
+
     // console.log("After  "+i,j);
-    
-  }
-  , 2000);
+  }, 2000);
 };
 slideShow();
